b91d2e066e743c2adea643b7a6a7f8d693968d0f
PedroBSB <pedro.melo.albuquerque@gmail.com>
2017-02-01 11:41:38 -0500

    Implement the generatePhi function.

--- src/RemoteSensing.cpp
+++ src/RemoteSensing.cpp
@@ -12,14 +12,14 @@ using namespace arma;
 /*********************************                      UTILS          *****************************************************/
 /***************************************************************************************************************************/
 
-NumericMatrix convertSBtoNormal(NumericMatrix vmat,
+arma::mat convertSBtoNormal(arma::mat vmat,
                                 int ncol, int nrow,
-                                NumericVector prod) {
-  NumericMatrix res(nrow,ncol);
+                                arma::vec prod) {
+  arma::mat res(nrow,ncol);
 
   for(int j=0; j<ncol;j++){
-    res(_,j)=vmat(_,j)*prod;
-    prod=prod*(1-vmat(_,j));
+    res.col(j)=vmat.col(j)*prod;
+    prod=prod*(1-vmat.col(j));
   }
 
   return (res);
@@ -111,8 +111,80 @@ arma::mat generateOmegaRemote(arma::mat thetaMat, arma::mat Omega, List jumpList
   return(Omega);
 }
 
+arma::mat generatePhiRemote(arma::mat Omega, arma::mat matX,arma::mat forestMat, List jumpList, arma::vec bPhi, double aPhi){
+  //Get the Jump Matrix for Omega
+  arma::mat jumpOmega = jumpList[0];
+  //Get the Jump Matrix for V
+  arma::mat jumpV = jumpList[1];
+  //Get the Jump Matrix for Omega
+  arma::mat jumpX = jumpList[2];
+  //Get the total number of Species
+  int n_species = matX.n_cols;
+  //Get the total number of communities
+  int n_community = matX.n_rows;
+  //Create the new matrix
+  arma::mat newXMat(matX.n_rows,matX.n_cols);
+  //Fill with ones
+  newXMat.fill(1.0);
+  //Create the new adjuested matrix
+  arma::mat xAdjust(matX.n_rows,matX.n_cols);
+  //Create the old prior matrix
+  arma::mat priorOld(matX.n_rows,matX.n_cols);
+  //Create the new prior matrix
+  arma::mat priorNew(matX.n_rows,matX.n_cols);
+  //Create the prod vector
+  arma::vec prod(n_community);
+  for(int c=0;c<n_community;c++)prod(c)=c;
+  //Initialize the matrix
+  arma::mat p1Old(1,1);
+  arma::mat p1New(1,1);
 
+  //For each number of species
+  for(int s=0;s<n_species;s++){
+    //For each community
+    for(int c=0;c<n_community;c++){
+      //Generate new X
+      newXMat(s,c) = tnormRemote(0.0, 1.0,matX(s,c), jumpX(s,c));
+      //Adjusting the Metropolis-Hasting
+      xAdjust(s,c) = fixMHRemote(0.0,1.0,matX(s,c),newXMat(s,c),jumpX(s,c));
+      //Old Phi
+      arma::mat phiOld=convertSBtoNormal(matX,n_species,n_community,prod);
+      //New Phi
+      arma::mat phiNew=convertSBtoNormal(newXMat,n_species,n_community,prod);
+      //Calculate the old probability
+      arma::mat pOld = Omega*phiOld;
+      //Calculate the new probability
+      arma::mat pNew = Omega*phiNew;
+      //Calculate the old scalar probability
+      p1Old = sum(forestMat*arma::log(pOld));
+      //Calculate the old scalar probability
+      p1New = arma::sum(forestMat*arma::log(pNew));
 
+      //Simulate old prior
+      priorOld(s,c)= R::dbeta(matX(s,c),aPhi,bPhi(s),true);
+      //Simulate new prior
+      priorNew(s,c)= R::dbeta(newXMat(s,c),aPhi,bPhi(s),true);
+
+      if(s<n_species-1){
+        //Probability p0
+        double p0 = p1Old(0,0)+priorOld(s,c);
+        //Probability p1
+        double p1 = p1New(0,0)+priorNew(s,c)+xAdjust(s,c);
+        //Acceptance
+        double a = std::exp(p1-p0);
+        double z = R::unif_rand();
+        if(z<a){
+          matX(s,c) = newXMat(s,c);
+        }
+      }
+    }
+  }
+
+  //Recreate the Phi Matrix
+  arma::mat phiMat = convertSBtoNormal(matX,n_species,n_community,prod);
+
+  return(phiMat);
+}
 
 
 

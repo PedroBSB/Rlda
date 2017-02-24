184176ad371195865cfe2db79c37f40c1fdd0bc3
PedroBSB <pedro.melo.albuquerque@gmail.com>
2017-02-24 13:22:16 -0500

    Delete unused files.

--- src/rgens.h
+++
@@ -1,9 +0,0 @@
-#ifndef RGENS_H
-#define RGENS_H
-arma::mat rmvnorm(unsigned int n, const arma::vec& mu, const arma::mat& S);
-
-arma::mat rwishart(unsigned int df, const arma::mat& S);
-
-arma::mat riwishart(unsigned int df, const arma::mat& S);
-  
-#endif
